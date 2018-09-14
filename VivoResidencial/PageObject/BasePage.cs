f87cb29a26c883cf10dd1d76e3af50be010d5395
Leandro Drumond Silveira Vianna <leandro.vianna@wooza.com.br>
2018-09-14 10:10:18 -0300

    Add project files.

---
+++ VivoResidencial/PageObject/BasePage.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VivoResidencial.PageObject {
+    public abstract class BasePage {
+
+        public IWebDriver driver;
+
+        protected BasePage(IWebDriver driver) {
+            this.driver = driver;
+        }
+
+        protected void Clicar(IWebElement element) {
+
+            if (element.Displayed) {
+
+                element.Click();
+
+            }
+        }
+
+
+        public void WaitSpinner() {
+
+            if (driver.FindElement(By.Id("spinner-content")).Displayed) {
+
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(50));
+                wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.Id("spinner-content")));
+            }
+        }
+
+
+    }
+
+
+
+
+
+}
---
+++ VivoResidencial/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VivoResidencial")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VivoResidencial")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bc000b9d-71e3-424f-ac9a-2825d84d38ed")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
